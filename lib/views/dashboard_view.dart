eb4343f7b4abae191263d822e93678f80a6da819
Ali Al-Jalo <ali.aljalo@gmail.com>
2024-09-14 17:43:38 +0300

    Create app style

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,18 @@
+import 'package:admin_dashboard/widgets/adaptive_layoutwidget.dart';
+import 'package:admin_dashboard/widgets/dashboard_desktop_layout.dart';
+import 'package:flutter/material.dart';
+
+class DashBoardView extends StatelessWidget {
+  const DashBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (ontext) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DashboardDesktopLayout(),
+      ),
+    );
+  }
+}
